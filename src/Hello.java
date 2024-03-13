b0a6d348f54372057e8c865e4c957d28fa7a1794
nojusbuc <noahmb.nmb@gmail.com>
2024-03-13 14:59:48 +0200

    conflicted sout in master branch

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("feature branch message");
+        System.out.println("conflicted message in master branch");
     }
 }
